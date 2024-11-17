4ab491c8088623415e6ab47a49f877888d68e72d
istebsharul <mdistebsharulbari@gmail.com>
2024-11-17 17:39:23 +0530

    CORS configured for admin and client

--- server/src/app.js
+++ server/src/app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: ['https://revline-rlux.onrender.com','https://revline-admin.onrender.com',], // Only allow your frontend domain
+    origin: ['https://revlineautoparts.com','https://admin.revlineautoparts.com',], // Only allow your frontend domain
     methods: ['GET', 'POST', 'PUT', 'DELETE'],   // Allowed methods
     credentials: true,                          // Allow cookies/auth headers
   }));
